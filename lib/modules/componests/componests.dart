ed34420aa1141e51a37f70840a7797da83430f89
Abd Alrhman Jamous <abdalrhman2001mm@gmail.com>
2024-04-21 16:05:23 +0300

    This new code

---
+++ lib/modules/componests/componests.dart
@@ -0,0 +1,63 @@
+
+import 'package:flutter/material.dart';
+
+ Widget defaultButton ({
+  double width =double.infinity,
+  Color background = Colors.black,
+  bool isUpprCsae =true,
+  double radius =0.0,
+  required String  text,
+   required Function function ,}) =>
+    Container(
+      width: width,
+      height: 50.0,
+
+      child: MaterialButton(
+          onPressed: ( ){function();},
+
+        child: Text(
+          isUpprCsae ? text.toUpperCase(): text,
+
+          style: TextStyle(
+            color:Colors.white,
+            fontSize: 30,
+          ),
+        ),
+
+      ),
+      decoration:BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          radius,
+        ),
+        color : background,
+
+      )
+    );
+Widget defoultbTextFormField(
+{
+ required TextEditingController controller,
+ required  TextInputType type,
+ required String ? Function (String? value) validate,
+  required String label,
+  required IconData prifix,
+  bool isPassword=false,
+   IconData? suffix,
+  Function ? suffixPresert,
+
+})=> TextFormField(
+controller: controller,
+keyboardType: type,
+  obscureText: isPassword ,
+  validator: ( String?value) =>validate (value),
+decoration: InputDecoration(
+
+ labelText:  label,
+  prefixIcon:Icon(prifix) ,
+  suffixIcon: suffix!=null? IconButton
+    (onPressed: () { suffixPresert!(); },
+    icon: Icon(suffix)
+  )
+      :null,
+border:OutlineInputBorder(),
+),
+);
---
+++ lib/social_app/social_media_app.dart
@@ -0,0 +1,129 @@
+
+import 'package:flutter/material.dart';
+import 'package:social_app/modules/componests/componests.dart';
+
+class loginscreen extends StatefulWidget {
+  @override
+  State<loginscreen> createState() => _loginscreenState();
+}
+
+class _loginscreenState extends State<loginscreen> {
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+  var formkey = GlobalKey<FormState>();
+  bool ispassword = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        leading: const Icon(
+          Icons.menu,
+        ),
+        title: Text('ABDALRHMAN'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.notification_important,
+            ),
+            onPressed: () {
+              print('abd');
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.search,
+            ),
+            onPressed: () {
+              print('abd');
+            },
+          ),
+        ],
+      ),
+      body:Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Form(
+          key: formkey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Login",
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20),
+
+              defoultbTextFormField(
+                controller: emailController,
+                label: 'Email',
+                prifix: Icons.email,
+                type: TextInputType.emailAddress,
+                validate: (value) {
+                  if (value!.isEmpty) {
+                    return 'password is must not be empty';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 10),
+
+              defoultbTextFormField(
+                  controller: passwordController,
+                  label: 'Password',
+                  prifix: Icons.lock,
+                  type: TextInputType.visiblePassword,
+                  isPassword: ispassword,
+                  suffix:
+                      ispassword ? Icons.visibility_outlined : Icons.visibility,
+                  suffixPresert: () {
+                    setState(() {
+                      ispassword = !ispassword;
+                    });
+                  },
+                  validate: (value) {
+                    if (value!.isEmpty) {
+                      return 'password is must not be empty';
+                    }
+                    return null;
+                  }),
+              SizedBox(height: 20),
+// هاد تابع مستعا من صفحة عامل فيها زر ال لوك ان
+              // ريسبل كومبوننت
+
+              defaultButton(
+                text: 'LogIN',
+                isUpprCsae: false,
+                //radius: 10.0,
+                function: () {
+                  if (formkey.currentState!.validate()) {
+                    print(emailController.text);
+                    print(passwordController.text);
+                  }
+                },
+              ),
+              SizedBox(height: 10),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Don`t have an account?'),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'now',
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
