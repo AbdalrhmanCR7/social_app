f173adc8bba04f3edc8093347c947daa9b0db864
Abd Alrhman Jamous <abdalrhman2001mm@gmail.com>
2024-05-16 12:01:27 +0300

    Edit firebase

---
+++ lib/features/feeds/presentation/page/feeds.dart
@@ -0,0 +1,311 @@
+
+import 'package:flutter/material.dart';
+
+class FeedsScreen extends StatelessWidget {
+  const FeedsScreen({super.key});
+
+  @override
+
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: SingleChildScrollView(
+
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            const Card(
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              elevation: 5.0,
+              margin: EdgeInsets.all(
+                8.0,
+              ),
+              child: Stack(
+                alignment: AlignmentDirectional.bottomEnd,
+                children: [
+                  Image(
+                    image: NetworkImage(
+                      'https://image.freepik.com/free-photo/horizontal-shot-smiling-curly-haired-woman-indicates-free-space-demonstrates-place-your-advertisement-attracts-attention-sale-wears-green-turtleneck-isolated-vibrant-pink-wall_273609-42770.jpg',
+                    ),
+                    fit: BoxFit.cover,
+                    height: 200.0,
+                    width: double.infinity,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'communicate with friends',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ListView.separated(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) => buildPostItem(context),
+              separatorBuilder: (context, index) => const SizedBox(
+                height: 8.0,
+              ),
+              itemCount: 10,
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+          ],
+        ),
+
+      ),
+    );
+  }
+  Widget buildPostItem(context) => Card(
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        elevation: 5.0,
+        margin: const EdgeInsets.symmetric(
+          horizontal: 8.0,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  const CircleAvatar(
+                    radius: 25.0,
+                    backgroundImage: NetworkImage(
+                      'https://image.freepik.com/free-photo/skeptical-woman-has-unsure-questioned-expression-points-fingers-sideways_273609-40770.jpg',
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 15.0,
+                  ),
+                  const Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              'Abdullah Mansour',
+                              style: TextStyle(
+                                height: 1.4,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 5.0,
+                            ),
+                          ],
+                        ),
+                        Text(
+                          'January 21, 2021 at 11:00 pm',
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 15.0,
+                  ),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.more_horiz,
+                      size: 16.0,
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 15.0,
+                ),
+                child: Container(
+                  width: double.infinity,
+                  height: 1.0,
+                  color: Colors.grey[300],
+                ),
+              ),
+              Text(
+                'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 10.0,
+                  top: 5.0,
+                ),
+                child: Container(
+                  width: double.infinity,
+                  child: Wrap(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsetsDirectional.only(
+                          end: 6.0,
+                        ),
+                        child: Container(
+                          height: 25.0,
+                          child: MaterialButton(
+                            onPressed: () {},
+                            minWidth: 1.0,
+                            padding: EdgeInsets.zero,
+                            child: const Text(
+                              '#software',
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsetsDirectional.only(
+                          end: 6.0,
+                        ),
+                        child: Container(
+                          height: 25.0,
+                          child: MaterialButton(
+                            onPressed: () {},
+                            minWidth: 1.0,
+                            padding: EdgeInsets.zero,
+                            child: const Text(
+                              '#flutter',
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                height: 140.0,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    4.0,
+                  ),
+                  image: const DecorationImage(
+                    image: NetworkImage(
+                      'https://image.freepik.com/free-photo/horizontal-shot-smiling-curly-haired-woman-indicates-free-space-demonstrates-place-your-advertisement-attracts-attention-sale-wears-green-turtleneck-isolated-vibrant-pink-wall_273609-42770.jpg',
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5.0,
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: InkWell(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 5.0,
+                          ),
+                          child: Row(
+                            children: [
+                              const Icon(
+                                Icons.favorite,
+                                size: 16.0,
+                                color: Colors.red,
+                              ),
+                              const SizedBox(
+                                width: 5.0,
+                              ),
+                              Text(
+                                '120',
+                                style: Theme.of(context).textTheme.caption,
+                              ),
+                            ],
+                          ),
+                        ),
+                        onTap: () {},
+                      ),
+                    ),
+                    Expanded(
+                      child: InkWell(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 5.0,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              const Icon(
+                                Icons.chat,
+                                size: 16.0,
+                                color: Colors.amber,
+                              ),
+                              const SizedBox(
+                                width: 5.0,
+                              ),
+                              Text(
+                                '120 comments',
+                                style: Theme.of(context).textTheme.caption,
+                              ),
+                            ],
+                          ),
+                        ),
+                        onTap: () {},
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 10.0,
+                ),
+                child: Container(
+                  width: double.infinity,
+                  height: 1.0,
+                  color: Colors.grey[300],
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: InkWell(
+                      child: const Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 18.0,
+                            backgroundImage: NetworkImage(
+                              'https://image.freepik.com/free-photo/skeptical-woman-has-unsure-questioned-expression-points-fingers-sideways_273609-40770.jpg',
+                            ),
+                          ),
+                          SizedBox(
+                            width: 15.0,
+                          ),
+                          Text(
+                            'Write a comment ...',
+                          ),
+                        ],
+                      ),
+                      onTap: () {},
+                    ),
+                  ),
+                  InkWell(
+                    child: Row(
+                      children: [
+                        const Icon(
+                          Icons.favorite_border,
+                          size: 16.0,
+                          color: Colors.red,
+                        ),
+                        const SizedBox(
+                          width: 5.0,
+                        ),
+                        Text(
+                          'LOVE',
+                          style: Theme.of(context).textTheme.caption,
+                        ),
+                      ],
+                    ),
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+}
--- lib/features/home/presentation/pages/home_screen.dart
+++ lib/features/home/presentation/pages/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:social_app/features/feeds/presentation/page/feeds.dart';
 import '../../../chat/presentation/pages/chat_screen.dart';
 import '../../../new_post/presentation/pages/new_post_screen.dart';
 import '../../../settings/presentation/pages/settings_screen.dart';
@@ -15,6 +15,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final PageController _pageController = PageController();
   final List<Widget> screens = const [
+    FeedsScreen(),
     ChatsScreen(),
     NewPostScreen(),
     UsersScreen(),
@@ -22,6 +23,7 @@ class _HomePageState extends State<HomePage> {
   ];
 
   final List<String> titles = [
+    'Home',
     'Chats',
     'Post',
     'Users',
@@ -31,87 +33,109 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
+    return
+      Scaffold(
+      body:
+      PageView(
         controller: _pageController,
         children: screens,
       ),
       bottomNavigationBar: Container(
         color: Colors.grey,
-        height: 65,
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      _pageController.animateToPage(
-                        0,
-                        duration: const Duration(milliseconds: 300),
-                        curve: Curves.easeIn,
-                      );
-                    },
-                    icon: const Icon(Icons.chat),
-                  ),
-                  Text(titles[0]),
-                ],
+        height: 60,
+           child: Row(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        _pageController.animateToPage(
+                          0,
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.easeIn,
+                        );
+                      },
+                      icon: const Icon(Icons.home_outlined),
+                    ),
+                    Text(titles[0]),
+                  ],
+                ),
               ),
-            ),
-            Expanded(
-              child: Column(
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      _pageController.animateToPage(
-                        1,
-                        duration: const Duration(milliseconds: 300),
-                        curve: Curves.easeIn,
-                      );
-                    },
-                    icon: const Icon(Icons.post_add),
-                  ),
-                  Text(titles[1]),
-                ],
+              Expanded(
+                child: Column(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        _pageController.animateToPage(
+                          1,
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.easeIn,
+                        );
+                      },
+                      icon: const Icon(Icons.post_add),
+                    ),
+                    Text(titles[1]),
+                  ],
+                ),
               ),
-            ),
-            Expanded(
-              child: Column(
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      _pageController.animateToPage(
-                        2,
-                        duration: const Duration(milliseconds: 300),
-                        curve: Curves.easeIn,
-                      );
-                    },
-                    icon: const Icon(Icons.person),
-                  ),
-                  Text(titles[2]),
-                ],
+              Expanded(
+                child: Column(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        _pageController.animateToPage(
+                          2,
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.easeIn,
+                        );
+                      },
+                      icon: const Icon(Icons.person),
+                    ),
+                    Text(titles[2]),
+                  ],
+                ),
               ),
-            ),
-            Expanded(
-              child: Column(
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      _pageController.animateToPage(
-                        3,
-                        duration: const Duration(milliseconds: 300),
-                        curve: Curves.easeIn,
-                      );
-                    },
-                    icon: const Icon(Icons.settings),
-                  ),
-                  Text(titles[3]),
-                ],
+              Expanded(
+                child: Column(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        _pageController.animateToPage(
+                          3,
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.easeIn,
+                        );
+                      },
+                      icon: const Icon(Icons.chat),
+                    ),
+                    Text(titles[3]),
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
-      ),
+              Expanded(
+                child: Column(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        _pageController.animateToPage(
+                          4,
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.easeIn,
+                        );
+                      },
+                      icon: const Icon(Icons.settings),
+                    ),
+                    Text(titles[4]),
+                  ],
+                ),
+              ),
+            ],
+                   ),
+         ),
+
+
     );
+
   }
 }
